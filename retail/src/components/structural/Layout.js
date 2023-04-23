97b817437ec84628d7a07a426588de3e197464b8
Youngwoo Kim <ykim846@wisc.edu>
2023-04-23 18:57:31 -0500

    layout css changed

--- retail/src/components/structural/Layout.js
+++ retail/src/components/structural/Layout.js
@@ -1,17 +1,16 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Form, FormControl, Nav, Navbar } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
 import React from 'react';
-
+import '../../navbar.css';
 import icon from '../../assets/leaf.svg'
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Layout(props) {
 
     return (
         <div>
-            <Navbar bg="primary" variant="primary">
+            <Navbar className="bar" expand="lg">
                 <Container>
                     <Navbar.Brand as={Link} to="/">
                         <img
@@ -21,17 +20,24 @@ function Layout(props) {
                             height="30"
                             className="d-inline-block align-top"
                         />{' '}
-                        Name
+                        
                     </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <>
-                        {
-                        //comment 
-                        }                   
-                        </>
-                        <Nav.Link as={Link} to="login">Login</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="navbar-nav" />
+                    <Navbar.Collapse id="navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link style={{ color: "#ff7f00" }} as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="login">Login</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <FormControl
+                                type="search"
+                                placeholder="Search"
+                                className="mr-2"
+                                aria-label="Search"
+                            />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </Form>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
             <div className="body-spacer">
